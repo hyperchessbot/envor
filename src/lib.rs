810b3382afbc3a1563491182cc8de03d372096d9
hyperchessbot <hyperchessbot@gmail.com>
2021-01-19 20:44:20 +0100

    $*

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,11 @@
 //!use envor::envor::*;
 //!
 //!fn main(){
-//!	println!("usage {:?}", Foo{});
+//!	let config_value:usize = env_or("CONFIG_VALUE", 100);
+//!
+//!	let flag:bool = env_or("SETTING", false);
+//!
+//!	println!("config value = {} , setting = {}", config_value, flag);
 //!}
 //!```
 //!
@@ -16,7 +20,10 @@
 //!use envor::envor::*;
 //!
 //!fn main(){
-//!	println!("advanced {:?}", Foo{});
+//!	let opt_value_ok:Option<i32> = env_or_opt("OPT_VALUE", -50);
+//!	let opt_value_illegal:Option<i32> = env_or_opt("OPT_VALUE", "illegal default");
+//!
+//!	println!("opt_value_ok = {:?} , opt_value_illegal = {:?}", opt_value_ok, opt_value_illegal);
 //!}
 //!```
 //!
